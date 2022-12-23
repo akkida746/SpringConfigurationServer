ef69615c5aa420f59f1fab20997abc8f74ef3948
deepaka <akash.deep@adidas.com>
2022-12-23 20:24:40 +0530

    Code cleanup

--- src/main/java/com/example/configurationserver/ConfigurationServerApplication.java
+++ src/main/java/com/example/configurationserver/ConfigurationServerApplication.java
@@ -1,24 +1,14 @@
 package com.example.configurationserver;
 
-import org.apache.hc.core5.reactor.Command;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.MutablePropertySources;
-
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.StreamSupport;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigurationServerApplication implements CommandLineRunner {
+public class ConfigurationServerApplication {
 
 	@Autowired
 	Environment environment;
@@ -27,20 +17,4 @@ public class ConfigurationServerApplication implements CommandLineRunner {
 		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
 
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		/*Map<String, Object> map =  ((AbstractEnvironment) environment).getSystemProperties();
-
-		final MutablePropertySources sources = ((AbstractEnvironment) environment).getPropertySources();
-		StreamSupport.stream(sources.spliterator(), false)
-				.filter(EnumerablePropertySource.class::isInstance)
-				.map(ps -> ((EnumerablePropertySource<?>) ps).getPropertyNames())
-				.flatMap(Arrays::stream)
-				.distinct()
-				.forEach(prop -> {
-					System.out.println(environment.getProperty(prop));
-				});*/
-	}
 }
